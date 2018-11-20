de72c2faa9071ce82eb83a5b83ab1ecbcb624c85
ossdeveloper <ossdeveloper8@gmail.com>
2018-11-20 16:48:14 +0530

    Updated readme.md

--- index.js
+++ index.js
@@ -9,3 +9,38 @@
 	exports.getAndTouchSingle = require('./lib/getAndTouchSingle').getAndTouchSingle
 	exports.getN1QLResults = require('./lib/getN1QLResults').getN1QlResults
 	
+	let db = require('./lib/buckets')
+	let buckets = {};
+	let getN1QLResults = require('./lib/getN1QLResults').getN1QlResults
+	
+	let username = 'vijay';
+	let password = 'bachi8';
+	let aryBuckets = ['logs'];
+	let aryIPs = ['195.201.127.0'];
+	
+	db.setupBuckets(username, password,aryBuckets,aryIPs)
+		.then((result) =>{
+			
+			if(result === true){
+				
+				buckets = db.getBuckets()
+				
+				let strQuery = 'select * from logs limit 10' // To get 10 records from default bucket
+				
+				let objBucket = db.getBuckets()['logs']
+				
+				getN1QLResults(strQuery, objBucket)
+					
+					.then((result) => console.log(result))
+					
+					.catch((e) => console.error(e))
+				
+			}else{
+				console.error(result)
+			}
+			
+		})
+		.catch((e) => {
+			console.error(e)
+		})
+
